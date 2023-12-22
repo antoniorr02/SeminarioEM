9c61d1d59a43d5bbd10267f56f51a617711bd56f
Antonio <antoniorr@correo.ugr.es>
2023-12-22 14:41:10 +0100

    regresion lineal

---
+++ LinearRegresionReservesExchangeRate.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+
+# Load the Excel file with exchange rate data
+excel_file_path_exchange = 'data/ExchangeRateData.xlsx'
+df_exchange_rate = pd.read_excel(excel_file_path_exchange)
+excel_file_path_reserves = 'data/ReservesData.xlsx'
+df_reserves = pd.read_excel(excel_file_path_reserves)
+
+# Assuming your Excel file has columns 'Date' and 'Exchange Rate'
+# Make sure to replace 'Date' and 'Exchange Rate' with your actual column names
+date_col = 'Date'
+exchange_rate_col = 'Exchange Rate'
+reserves = 'Total Reserves'
+
+# Assuming you have two pandas DataFrames df_exchange_rate_exchange_rate and df_exchange_rate_reserves
+# with columns 'Date' and 'Exchange Rate' for the exchange rate
+# and 'Date' and 'International Reserves' for the reserves
+
+# Convert 'Date' columns to datetime format
+df_exchange_rate['Date'] = pd.to_datetime(df_exchange_rate['Date'], format='%d/%m/%Y')
+df_reserves['Date'] = pd.to_datetime(df_reserves['Date'], format='%d/%m/%Y')
+
+df_exchange_rate_monthly = df_exchange_rate.resample('M', on='Date').mean().reset_index()
+
+# Merge the two DataFrames on the 'Date' column
+merged_df_exchange_rate = pd.merge(df_exchange_rate_monthly, df_reserves, on='Date', how='inner')
+
+# Calculate the daily change in exchange rate
+merged_df_exchange_rate['Exchange Rate Change'] = merged_df_exchange_rate['Exchange Rate'].diff()
+
+# Drop the NaN value resulting from the diff() operation
+merged_df_exchange_rate = merged_df_exchange_rate.dropna()
+
+# Select features and target variable
+X = merged_df_exchange_rate[['Exchange Rate Change']]
+y = merged_df_exchange_rate['Total Reserves']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a linear regression model
+model = LinearRegression()
+
+# Train the model
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+mse = mean_squared_error(y_test, y_pred)
+print(f'Mean Squared Error: {mse}')
+
+# Plot the original data and the linear regression line
+plt.scatter(X_test, y_test, color='blue', label='Actual Reserves')
+plt.plot(X_test, y_pred, color='red', linewidth=2, label='Linear Regression Prediction')
+plt.xlabel('Exchange Rate Change')
+plt.ylabel('International Reserves')
+plt.legend()
+plt.title('Linear Regression: Exchange Rate Change vs International Reserves')
+plt.show()
--- main.py
+++ main.py
@@ -24,13 +24,13 @@ def run_script(script_path):
     threads.append(thread)
 
 def small_atack():
-    #Componente raíz
+    #Root Component
     submenu = tkinter.Toplevel(raiz)
     submenu.title("Small Atacks")
     submenu.geometry('500x350')
     submenu.resizable(False,False)
 
-    #Botones submenu
+    #Buttons submenu
     botonA= tkinter.Button(submenu, text="Attacks 0.05", width=50, height=2, command=lambda: run_script('SmallAtacks.py'))
     botonA.grid(row=1, column=3, columnspan=1, padx=35, pady=10)
     botonB= tkinter.Button(submenu, text="Attacks 0.1", width=50, height=2, command=lambda: run_script('SmallAtacks.py'))
@@ -43,26 +43,28 @@ def small_atack():
     botonClose.grid(row=5, column=3, columnspan=1, padx=35, pady=10)
 
 
-#Componente raíz
+#Root Component
 raiz = tkinter.Tk()
 raiz.title("Danger theory applied to economics")
-raiz.geometry('500x420')
+raiz.geometry('500x480')
 raiz.resizable(False,False)
 
 threads = []  # List to store the threads
 
-#Botones
+#Buttons
 boton1 = tkinter.Button(raiz, text="Real Exchange Rate", width=50, height=2, command=lambda: run_script('RealExchangeRate.py'))
 boton1.grid(row=2, column=3, columnspan=1, padx=35, pady=10)
 boton2 = tkinter.Button(raiz, text="International GPB Reserves", width=50, height=2, command=lambda: run_script('Reserves.py'))
 boton2.grid(row=3, column=3, columnspan=1, padx=35, pady=10)
-boton3= tkinter.Button(raiz, text="Fluctuation Band 2.25%", width=50, height=2, command=lambda: run_script('FluctuationBand.py'))
+boton3 = tkinter.Button(raiz, text="Lineal Regresion", width=50, height=2, command=lambda: run_script('LinearRegresionReservesExchangeRate.py'))
 boton3.grid(row=4, column=3, columnspan=1, padx=35, pady=10)
-boton4= tkinter.Button(raiz, text="Fluctuation Band 15%", width=50, height=2, command=lambda: run_script('FluctuationBand_15.py'))
+boton4= tkinter.Button(raiz, text="Fluctuation Band 2.25%", width=50, height=2, command=lambda: run_script('FluctuationBand.py'))
 boton4.grid(row=5, column=3, columnspan=1, padx=35, pady=10)
-boton5= tkinter.Button(raiz, text="Small Attacks", width=50, height=2, command=small_atack)
+boton5= tkinter.Button(raiz, text="Fluctuation Band 15%", width=50, height=2, command=lambda: run_script('FluctuationBand_15.py'))
 boton5.grid(row=6, column=3, columnspan=1, padx=35, pady=10)
-boton6= tkinter.Button(raiz, text="Close", width=50, height=2, command=stop)
+boton6= tkinter.Button(raiz, text="Small Attacks", width=50, height=2, command=small_atack)
 boton6.grid(row=7, column=3, columnspan=1, padx=35, pady=10)
+boton6= tkinter.Button(raiz, text="Close", width=50, height=2, command=stop)
+boton6.grid(row=8, column=3, columnspan=1, padx=35, pady=10)
 
 raiz.mainloop()
